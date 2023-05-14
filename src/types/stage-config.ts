b30e13c13ccd1e6b6257a3f5398672531a4dfe92
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2023-05-15 00:40:52 +0200

    Dont lock the consumer to use the predefined app stages, make them available via Default_APP_Stage enum

--- src/types/stage-config.ts
+++ src/types/stage-config.ts
@@ -1,13 +1,15 @@
-export type APP_Stage = 'PROD' | 'INT' | 'TEST' | 'LA Beta'
+export type Default_APP_Stage = 'PROD' | 'INT' | 'TEST' | 'LA Beta'
 
 export interface StageConfig {
-    key: APP_Stage
+    key: string
     apiEndpoint: string
     authBearerKey: string
 }
 
+const defaultStageKey: Default_APP_Stage = 'PROD'
+
 export const DEFAULT_STAGE: StageConfig = {
-    key: 'PROD',
+    key: defaultStageKey,
     apiEndpoint: 'https://api.opentransportdata.swiss/ojp2020',
     authBearerKey: '57c5dbbbf1fe4d00010000186ba6e4bb4be543a9b4e40d2d6495592b',
 }
