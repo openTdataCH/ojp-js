96f00ecdc180513e1bd3600d4c3fcf4c292bf338
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2023-10-08 14:22:22 +0200

    Adds new POI categories

--- src/types/geo-restriction.type.ts
+++ src/types/geo-restriction.type.ts
@@ -1,5 +1,5 @@
 export type GeoRestrictionType = 'stop' | 'address' | 'poi_amenity' | 'poi_all' | 'coord' | 'topographicPlace'
 
 type PoiOSMTagAmenity = 'escooter_rental' | 'car_sharing' | 'bicycle_rental' | 'charging_station'
-type PoiOSMTagPOI = 'service' | 'shopping' | 'leisure' | 'catering' | 'public' | 'parkride' | 'accommodation'
+type PoiOSMTagPOI = 'service' | 'shopping' | 'leisure' | 'catering' | 'public' | 'parkride' | 'accommodation' | 'sbb_services' | 'other'
 export type GeoRestrictionPoiOSMTag = PoiOSMTagAmenity | PoiOSMTagPOI
