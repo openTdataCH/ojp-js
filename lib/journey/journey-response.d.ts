762f91c24801d095cd2421d23aa69984069c0ac7
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-02-28 11:01:13 +0100

    cleanup /lib

--- lib/journey/journey-response.d.ts
+++
@@ -1,8 +0,0 @@
-import { Trip } from "../trip";
-import { JourneySection } from "./journey-section";
-export declare class JourneyResponse {
-    sections: JourneySection[];
-    constructor(sections: JourneySection[]);
-    computeAggregatedTrips(): Trip[];
-    private computeFirstValidTrip;
-}
--- lib/journey/journey-section.d.ts
+++
@@ -1,7 +0,0 @@
-import { RequestData } from "../request";
-import { TripsResponse } from "../trips/trips-response";
-export declare class JourneySection {
-    requestData: RequestData;
-    response: TripsResponse;
-    constructor(requestData: RequestData, response: TripsResponse);
-}
