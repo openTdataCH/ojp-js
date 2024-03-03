293862a7805d19df6b8c47aec6a52e89f4f2077b
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-03-03 09:56:05 +0100

    Adds poc app for TripRequest pagination

--- examples/trips-pagination/src/app/app.module.ts
+++ examples/trips-pagination/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
