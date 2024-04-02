a0c561a657ff6f9842d4cec27aebcb421712ebb0
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-04-02 14:03:21 +0200

    Change the constructor

--- src/location/address.ts
+++ src/location/address.ts
@@ -3,12 +3,14 @@ import { TreeNode } from "../xml/tree-node";
 export class Address {
   public addressCode: string
   public addressName: string | null
+
   public topographicPlaceRef: string | null
 
-  constructor(addressCode: string, addressName: string | null, topographicPlaceRef: string | null) {
+  constructor(addressCode: string) {
     this.addressCode = addressCode
-    this.addressName = addressName
-    this.topographicPlaceRef = topographicPlaceRef
+    
+    this.addressName = null;
+    this.topographicPlaceRef = null;
   }
 
   public static initWithLocationTreeNode(locationTreeNode: TreeNode): Address | null {
@@ -22,10 +24,11 @@ export class Address {
       return null
     }
 
-    const addressName = addressTreeNode.findTextFromChildNamed('AddressName/Text')
-    const topographicPlaceRef = addressTreeNode.findTextFromChildNamed('TopographicPlaceRef')
+    const address = new Address(addressCode);
 
-    const address = new Address(addressCode, addressName, topographicPlaceRef);
+    address.addressName = addressTreeNode.findTextFromChildNamed('AddressName/Text')
+
+    address.topographicPlaceRef = addressTreeNode.findTextFromChildNamed('TopographicPlaceRef');
 
     return address;
   }
