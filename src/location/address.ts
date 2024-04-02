5258b18fecf97e995a4f864af3902b4d30c023a9
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-04-02 14:03:53 +0200

    Adds more properties, all optional

--- src/location/address.ts
+++ src/location/address.ts
@@ -5,12 +5,21 @@ export class Address {
   public addressName: string | null
 
   public topographicPlaceRef: string | null
+  public topographicPlaceName: string | null
+
+  public street: string | null
+  public houseNumber: string | null
+  public postCode: string | null
 
   constructor(addressCode: string) {
     this.addressCode = addressCode
     
     this.addressName = null;
     this.topographicPlaceRef = null;
+    this.topographicPlaceName = null;
+    this.street = null;
+    this.houseNumber = null;
+    this.postCode = null;
   }
 
   public static initWithLocationTreeNode(locationTreeNode: TreeNode): Address | null {
@@ -29,6 +38,11 @@ export class Address {
     address.addressName = addressTreeNode.findTextFromChildNamed('AddressName/Text')
 
     address.topographicPlaceRef = addressTreeNode.findTextFromChildNamed('TopographicPlaceRef');
+    address.topographicPlaceName = addressTreeNode.findTextFromChildNamed('TopographicPlaceName');
+    
+    address.street = addressTreeNode.findTextFromChildNamed('Street');
+    address.houseNumber = addressTreeNode.findTextFromChildNamed('HouseNumber');
+    address.postCode = addressTreeNode.findTextFromChildNamed('PostCode');    
 
     return address;
   }
