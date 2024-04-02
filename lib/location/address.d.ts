7d2c1a5f77e011184d2437e7015d633c8e63d464
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-04-02 14:05:55 +0200

    New build

--- lib/location/address.d.ts
+++ lib/location/address.d.ts
@@ -3,6 +3,10 @@ export declare class Address {
     addressCode: string;
     addressName: string | null;
     topographicPlaceRef: string | null;
-    constructor(addressCode: string, addressName: string | null, topographicPlaceRef: string | null);
+    topographicPlaceName: string | null;
+    street: string | null;
+    houseNumber: string | null;
+    postCode: string | null;
+    constructor(addressCode: string);
     static initWithLocationTreeNode(locationTreeNode: TreeNode): Address | null;
 }
