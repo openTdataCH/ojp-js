1d6883d7416cce0dd8c9cd9306fb7ed362da5653
gobeli <etienne.gobeli@outlook.com>
2024-06-26 11:12:32 +0200

    resolve circular dependency

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const SDK_VERSION = '0.9.35';
+export const IS_NODE_CLI = typeof process !== 'undefined' && process.versions && process.versions.node;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export const SDK_VERSION = '0.9.35';
-export const IS_NODE_CLI = typeof process !== 'undefined' && process.versions && process.versions.node;
-
 export * from './config/map-colors'
 
 export * from './fare/fare'
