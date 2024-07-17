8099a6c8bd3ee8215d03c0678b8e0e3aeafb736e
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-07-17 15:26:13 +0200

    These are in ojp: namespace

--- src/request/trip-info-request/trip-info-request-params.ts
+++ src/request/trip-info-request/trip-info-request-params.ts
@@ -27,8 +27,8 @@ export class TripInfoRequestParams extends BaseRequestParams {
         const requestNode = this.serviceRequestNode.ele('OJPTripInfoRequest');
         requestNode.ele('siri:RequestTimestamp', dateNowF);
 
-        requestNode.ele('siri:JourneyRef', this.journeyRef);
-        requestNode.ele('siri:OperatingDayRef', this.operatingDayRef);
+        requestNode.ele('JourneyRef', this.journeyRef);
+        requestNode.ele('OperatingDayRef', this.operatingDayRef);
 
         const paramsNode = requestNode.ele('Params');
         paramsNode.ele('IncludeCalls', true);
