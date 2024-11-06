e1826be940d303e2fa5c5463331b02f29ebfc20d
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-11-06 16:20:08 +0100

    Adds ModeOfTransportType

---
+++ src/types/mode-of-transport.type.ts
@@ -0,0 +1,7 @@
+// https://vdvde.github.io/OJP/develop/documentation-tables/siri.html#type_siri__VehicleModesOfTransportEnumeration
+export type ModeOfTransportType = 'air' |
+    'bus' | 'coach' | 'trolleyBus' |
+    'metro' | 'rail' | 'tram' | 
+    'water' | 'ferry' |
+    'cableway' | 'funicular' | 'lift' |
+    'other' | 'unknown';
