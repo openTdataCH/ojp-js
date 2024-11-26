cbc80ed326571d07d4e5b235b8ed43289b3863a3
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2024-11-26 14:41:51 +0100

    npm run build

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -1,5 +1,5 @@
 type DEBUG_LEVEL_Type = 'DEBUG' | 'PROD';
 export declare const DEBUG_LEVEL: DEBUG_LEVEL_Type;
-export declare const SDK_VERSION = "0.13.1";
+export declare const SDK_VERSION = "0.13.2";
 export declare const IS_NODE_CLI: string | false;
 export {};
