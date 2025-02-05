3409a821476170f9b4a59ed3c9a60d46b0171875
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-02-05 16:56:07 +0100

    Actually we dont need a default stage for Nova

--- src/types/stage-config.ts
+++ src/types/stage-config.ts
@@ -13,9 +13,3 @@ export const DEFAULT_STAGE: StageConfig = {
     apiEndpoint: 'https://api.opentransportdata.swiss/ojp2020',
     authBearerKey: '57c5dbbbf1fe4d00010000186ba6e4bb4be543a9b4e40d2d6495592b',
 }
-
-export const FARES_API_DEFAULT_STAGE: StageConfig = {
-    key: defaultStageKey,
-    apiEndpoint: 'https://api.opentransportdata.swiss/ojpfare',
-    authBearerKey: 'OJPFare_Key',
-}
