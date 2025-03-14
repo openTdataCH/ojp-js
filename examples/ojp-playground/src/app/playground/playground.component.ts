bd57fa75460b333dff79a966a9bfcaa88d9ea3c3
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-03-14 17:04:55 +0100

    Enable TR (parts)

--- examples/ojp-playground/src/app/playground/playground.component.ts
+++ examples/ojp-playground/src/app/playground/playground.component.ts
@@ -25,7 +25,7 @@ export class PlaygroundComponent implements OnInit {
 
   private async runExamples() {
     await this.runLIR();
-    // await this.runTR();
+    await this.runTR();
     // await this.runSER();
   }
 
@@ -69,72 +69,53 @@ export class PlaygroundComponent implements OnInit {
     console.log(placeResults3);
   }
 
-  // private async runTR() {
-  //   console.log('======================');
-  //   console.log('TR Requests');
-  //   console.log('======================');
+  private async runTR() {
+    console.log('======================');
+    console.log('TR Requests');
+    console.log('======================');
 
-  //   // Building request
+    // Building request
 
-  //   const fromStopRef = '8507000';  // Bern
-  //   const toStopRef = '8503000';    // Zürich
+    const fromStopRef = '8507000';  // Bern
+    const toStopRef = '8503000';    // Zürich
 
-  //   const fromLocation = OJP.Location.initWithStopPlaceRef(fromStopRef); 
-  //   const toLocation = OJP.Location.initWithStopPlaceRef(toStopRef); 
+    const request1 = OJP.TripRequest.initWithPlaceRefsAndDate(fromStopRef, toStopRef, new Date());
 
-  //   const request1 = OJP.TripRequest.initWithLocationsAndDate(OJP.DEFAULT_STAGE, fromLocation, toLocation, new Date(), 'Dep');
-  //   if (request1 === null) {
-  //     // handle invalid requests
-  //     return;
-  //   }
-
-  //   // This is equivalent with request1
-  //   const request1_B = OJP.TripRequest.initWithStopRefs(OJP.DEFAULT_STAGE, fromStopRef, toStopRef, new Date(), 'Dep');
-
-  //   // Request with long/lat coordinates
-  //   // https://opentdatach.github.io/ojp-demo-app/search?from=46.957522,7.431170&to=46.931849,7.485132
-  //   const fromLocationCoords = OJP.Location.initWithLngLat(7.431170, 46.957522);
-  //   const toLocationCoords = OJP.Location.initWithLngLat(7.485132, 46.931849);
-  //   const request2 = OJP.TripRequest.initWithLocationsAndDate(OJP.DEFAULT_STAGE, fromLocationCoords, toLocationCoords, new Date(), 'Dep');
+    // TBA
+    // // Request with long/lat coordinates
+    // // https://opentdatach.github.io/ojp-demo-app/search?from=46.957522,7.431170&to=46.931849,7.485132
+    // const fromLocationCoords = OJP.Location.initWithLngLat(7.431170, 46.957522);
+    // const toLocationCoords = OJP.Location.initWithLngLat(7.485132, 46.931849);
+    // const request2 = OJP.TripRequest.initWithLocationsAndDate(OJP.DEFAULT_STAGE, fromLocationCoords, toLocationCoords, new Date(), 'Dep');
     
-  //   // Handling response
+    // Handling response
     
-  //   // a) using await/async
-  //   const response1 = await request1.fetchResponse();
-  //   console.log('a) TR using await/async')
-  //   console.log(response1);
+    // a) using await/async
+    const response1 = await this.ojpSDK.fetchTrips(request1);
+    console.log('a) TR using await/async')
+    console.log(response1);
 
-  //   // b) using Promise.then
-  //   request1.fetchResponse().then(response => {
-  //     console.log('b) TR using Promise.then')
-  //     console.log(response);
-  //   });
-
-  //   // c) using a callback
-  //   // the XML parsing might some time for processing therefore using a callback can allow the GUI to react quickly when having partial results
-  //   request1.fetchResponseWithCallback(response => {
-  //     if (response.message === 'TripRequest.DONE') {
-  //       // all trips were parsed, this is also fired when using Promise.then approach
-  //       console.log('c) TR using callback')
-  //       console.log(response);
-  //     } else {
-  //       if (response.message === 'TripRequest.TripsNo') {
-  //         // logic how to proceed next, have an idea of how many trips to expect
-  //         // console.log(response);
-  //       }
+    // TBA - not sure if needed now
+    // // c) using a callback
+    // // the XML parsing might some time for processing therefore using a callback can allow the GUI to react quickly when having partial results
+    // request1.fetchResponseWithCallback(response => {
+    //   if (response.message === 'TripRequest.DONE') {
+    //     // all trips were parsed, this is also fired when using Promise.then approach
+    //     console.log('c) TR using callback')
+    //     console.log(response);
+    //   } else {
+    //     if (response.message === 'TripRequest.TripsNo') {
+    //       // logic how to proceed next, have an idea of how many trips to expect
+    //       // console.log(response);
+    //     }
         
-  //       if (response.message === 'TripRequest.Trip') {
-  //         // handle trip by trip, this is faster than expecting for whole TripRequest.DONE event
-  //         // console.log(response);
-  //       }
-  //     }
-  //   });
-
-  //   request2?.fetchResponse().then(response => {
-  //     console.log('TR with coordinates');
-  //     console.log(response);
-  //   });
-  // }
+    //     if (response.message === 'TripRequest.Trip') {
+    //       // handle trip by trip, this is faster than expecting for whole TripRequest.DONE event
+    //       // console.log(response);
+    //     }
+    //   }
+    // });
+  }
 
   // private async runSER() {
   //   console.log('======================');
