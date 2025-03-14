9f395fbf40e9acc97e01b265aaf04a8b5669c9f3
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-03-14 15:31:19 +0100

    Adds entry point

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// These exports - ./types/openapi - should be kept at minimum as possible
+export { 
+  TimedLegSchema as TimedLeg, 
+  TransferLegSchema as TransferLeg, 
+  ContinuousLegSchema as ContinuousLeg,
+} from './types/openapi';
+
+export { HTTPConfig } from "./types/_all";
