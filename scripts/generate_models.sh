0331b0bc10c198c08c7e70be001a516f30645e11
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-03-14 15:16:47 +0100

    Adds models generation script

---
+++ scripts/generate_models.sh
@@ -0,0 +1,48 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+APP_PATH=$DIR/..
+
+OPENAPI_YAML_PATH=$APP_PATH/openapi
+OPENAPI_GENERATED_TS_PATH=$APP_PATH/openapi/generated
+
+SRC_FILES=(
+    "ojp-trip-request.yaml"
+    "ojp-trip-response.yaml"
+    "ojp-location-request.yaml"
+    "ojp-location-response.yaml"
+)
+for file in "${SRC_FILES[@]}"; do
+    filename="${file%.*}"
+    src_path=$OPENAPI_YAML_PATH/$file
+    bundle_src_path=$OPENAPI_YAML_PATH/$filename.gen.bundle.yaml
+    dst_path=$OPENAPI_GENERATED_TS_PATH/$filename.ts
+
+    # 1. merge the YAML files into one
+    npx --prefix $APP_PATH \
+        swagger-cli bundle $src_path \
+        -o $bundle_src_path \
+        --type yaml
+
+    # 2. convert to TS
+    npx --prefix $APP_PATH \
+        openapi-typescript $bundle_src_path \
+        -o $dst_path
+
+    # 3. cleanup
+    rm -f $bundle_src_path
+    
+    # Print the filename without extension
+    echo "$file exported to $dst_path"
+    echo ""
+done
+
+SRC_FILES=("shared.yml")
+for file in "${SRC_FILES[@]}"; do
+    src_path=$OPENAPI_YAML_PATH/$file
+    
+    filename="${file%.*}"
+    dst_path=$OPENAPI_GENERATED_TS_PATH/$filename.ts
+    
+    npx --prefix $APP_PATH \
+        openapi-typescript $src_path \
+        -o $dst_path
+done
