c8dcc2c5a7f485b1adf3246c556892e753c9b8bd
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-03-19 21:25:34 +0100

    Adds helper for formatting date

--- src/helpers/date-helpers.ts
+++ src/helpers/date-helpers.ts
@@ -23,4 +23,18 @@ export class DateHelpers {
     const dateFormatted = DateHelpers.formatDate(d)
     return dateFormatted.substring(11,16);
   }
+
+  public static computeDelayMinutes(timetableTimeS: Date | string, estimatedTimeS: Date | string | null): number | null {
+    if (estimatedTimeS === null) {
+      return null;
+    }
+
+    const timetableTime = typeof timetableTimeS === 'string' ? new Date(timetableTimeS) : timetableTimeS;
+    const estimatedTime = typeof estimatedTimeS === 'string' ? new Date(estimatedTimeS) : estimatedTimeS;
+
+    const dateDiffSeconds = (estimatedTime.getTime() - timetableTime.getTime()) / 1000;
+    const delayMinutes = Math.floor(dateDiffSeconds / 60);
+
+    return delayMinutes;
+  }
 }
