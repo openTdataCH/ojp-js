65ec2426de88b26ba9aaa5eec044e6ef27070ff2
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-02 12:58:21 +0200

    npm build

--- lib/config/map-colors.d.ts
+++
@@ -1,6 +0,0 @@
-import { LegType } from "../trip";
-import { TripLegLineType } from "../types/map-geometry-types";
-export declare const MapLegTypeColor: Record<LegType, string>;
-export declare const MapLegTypes: LegType[];
-export declare const MapLegLineTypeColor: Record<TripLegLineType, string>;
-export declare const MapTripLegLineTypes: TripLegLineType[];
