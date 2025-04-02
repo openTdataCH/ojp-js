e1fdc885d9cfa749edd0690fed0826a323b4e5b7
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-02 12:50:25 +0200

    Updates lib

--- lib/shared/duration.d.ts
+++ lib/shared/duration.d.ts
@@ -3,7 +3,7 @@ export declare class Duration {
     hours: number;
     minutes: number;
     totalMinutes: number;
-    constructor(hours: number, minutes: number);
+    private constructor();
     static initWithTreeNode(parentTreeNode: TreeNode, nodeName?: string): Duration | null;
     static initFromDurationText(durationS: string | null): Duration | null;
     static initFromTotalMinutes(totalMinutes: number): Duration;
