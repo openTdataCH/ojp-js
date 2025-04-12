9aa04f904f93d02fb711a86c1c6dc7cce9103ac4
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-12 21:36:59 +0200

    Adds DefaultXML_Config

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,12 @@
+import { XML_Config } from "./types/_all.js";
+
 export const SDK_VERSION = '0.20.2';
+
+export const DefaultXML_Config: XML_Config = {
+  version: '2.0',
+  defaultNS: 'ojp',
+  mapNS: {
+    'ojp': 'http://www.vdv.de/ojp',
+    'siri': 'http://www.siri.org.uk/siri',
+  },
+};
