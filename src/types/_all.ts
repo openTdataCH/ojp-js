4e493016a6c4c36be6bc6a782fe71a601646f5c7
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-12 21:36:34 +0200

    Adds XML_Config type

--- src/types/_all.ts
+++ src/types/_all.ts
@@ -4,3 +4,9 @@ export interface HTTPConfig {
 }
 
 export type Language = 'de' | 'fr' | 'it' | 'en';
+
+export interface XML_Config {
+  version: '1.0' | '2.0',
+  defaultNS: 'ojp' | 'siri' | null,
+  mapNS: Record<string, string>
+}
