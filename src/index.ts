afa2cc9cd70def8e4e8dd4111cf146034f599e9b
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-15 14:26:48 +0200

    Expose TripRefineRequest

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export {
   LocationInformationRequest, 
   StopEventRequest, 
   TripRequest,
+  TripRefineRequest,
 } from './models/request';
 
 export { GeoPosition } from './models/geoposition';
