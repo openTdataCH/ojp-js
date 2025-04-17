44a2b124f85c52f2edd4219be2e296a3d3c3a3da
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-17 21:50:14 +0200

    Adds XML_Config for OJP v1,2

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { XML_Config } from "./types/_all";
+
 type DEBUG_LEVEL_Type = 'DEBUG' | 'PROD';
 export const DEBUG_LEVEL: DEBUG_LEVEL_Type = (() => {
     const isBrowser = typeof window !== 'undefined' && typeof window.document !== 'undefined';
@@ -22,3 +24,22 @@ if (DEBUG_LEVEL === 'DEBUG') {
     console.log('OJP-JS SDK version : ' + SDK_VERSION);
     console.log('OJP-SDK            : DEBUG features are enabled');
 }
+
+export const XML_Config_OJPv1: XML_Config = {
+    ojpVersion: '1.0',
+    defaultNS: null,
+    mapNS: {
+        'ojp': 'http://www.vdv.de/ojp',
+        'siri': 'http://www.siri.org.uk/siri',
+    },
+};
+
+export const XML_Config_OJPv2: XML_Config = {
+    ojpVersion: '2.0',
+    defaultNS: 'ojp',
+    mapNS: {
+        'ojp': 'http://www.vdv.de/ojp',
+        'siri': 'http://www.siri.org.uk/siri',
+    },
+};
+
