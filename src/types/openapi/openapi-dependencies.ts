1a1779ce663190629215a814f0a15643c59c3e54
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-17 18:37:38 +0200

    Enforce namespaces

--- src/types/openapi/openapi-dependencies.ts
+++ src/types/openapi/openapi-dependencies.ts
@@ -28,7 +28,15 @@ export const MapNS_Tags: Record<string, string> = {
   'LegIntermediate.StopPointRef': 'siri',
   'LegAlight.StopPointRef': 'siri',
   
+  'Mode.AirSubmode': 'siri',
+  'Mode.BusSubmode': 'siri',
+  'Mode.CoachSubmode': 'siri',
+  'Mode.FunicularSubmode': 'siri',
+  'Mode.MetroSubmode': 'siri',
+  'Mode.TramSubmode': 'siri',
+  'Mode.TelecabinSubmode': 'siri',
   'Mode.RailSubmode': 'siri',
+  'Mode.WaterSubmode': 'siri',
 
   'Service.LineRef': 'siri',
   'Service.OperatorRef': 'siri',
