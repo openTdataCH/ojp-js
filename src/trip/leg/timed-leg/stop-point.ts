cc621e4d5f65964ccd19d7f5ef6f68dd6dca2cf3
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-18 16:41:59 +0200

    This is under siri:

--- src/trip/leg/timed-leg/stop-point.ts
+++ src/trip/leg/timed-leg/stop-point.ts
@@ -78,7 +78,7 @@ export class StopPoint {
     stopPoint.siriSituationIds = [];
     const situationFullRefTreeNodes = treeNode.findChildrenNamed('SituationFullRef');
     situationFullRefTreeNodes.forEach(situationFullRefTreeNode => {
-      const situationNumber = situationFullRefTreeNode.findTextFromChildNamed('SituationNumber');
+      const situationNumber = situationFullRefTreeNode.findTextFromChildNamed('siri:SituationNumber');
       if (situationNumber) {
         stopPoint.siriSituationIds.push(situationNumber);
       }
