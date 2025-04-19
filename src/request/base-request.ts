4b5060989cf26b45710ddcb7a558b9ded40c81af
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-04-19 14:37:59 +0200

    Use the builder

--- src/request/base-request.ts
+++ src/request/base-request.ts
@@ -5,7 +5,7 @@ import { ApiConfig } from '../types/stage-config';
 import { RequestInfo } from './types/request-info.type';
 import { Language } from '../types/language-type';
 import { XML_Config } from '../types/_all';
-import { REQUESTOR_REF, XML_Config_Default } from '../constants';
+import { REQUESTOR_REF, XML_BuilderConfig } from '../constants';
 
 export class OJPBaseRequest {
   private stageConfig: ApiConfig;
@@ -20,7 +20,7 @@ export class OJPBaseRequest {
   protected mockRequestXML: string | null;
   protected mockResponseXML: string | null;
 
-  constructor(stageConfig: ApiConfig, language: Language, xmlConfig: XML_Config = XML_Config_Default, requestorRef = REQUESTOR_REF) {
+  constructor(stageConfig: ApiConfig, language: Language, xmlConfig: XML_Config = XML_BuilderConfig, requestorRef = REQUESTOR_REF) {
     this.stageConfig = stageConfig;
     this.language = language;
     this.xmlConfig = xmlConfig;
