d3b2646edefc6f4ccdcdaccbdddbbae660d3f146
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-05-02 14:45:21 +0200

    npm run build

--- lib/types/individual-mode.types.d.ts
+++ lib/types/individual-mode.types.d.ts
@@ -2,5 +2,5 @@ type DefaultIndividualTransportMode = 'public_transport' | 'walk' | 'foot' | 'cy
 type SharedIndividualTransportMode = 'escooter_rental' | 'car_sharing' | 'self-drive-car' | 'bicycle_rental';
 type OtherTransportMode = 'charging_station' | 'taxi' | 'others-drive-car' | 'car-shuttle-train' | 'car-ferry';
 export type IndividualTransportMode = DefaultIndividualTransportMode | SharedIndividualTransportMode | OtherTransportMode;
-export type TransferMode = 'walk' | 'remainInVehicle';
+export type TransferMode = 'walk' | 'remainInVehicle' | 'changeWithinVehicle';
 export {};
