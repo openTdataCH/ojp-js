66bca8564791efd8321e47d0cf1969f60985ec78
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-05-14 21:19:56 +0200

    Updates patch number

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { XML_Config } from "./types/_all";
 
-export const SDK_VERSION = '0.20.13';
+export const SDK_VERSION = '0.20.14';
 
 export const DefaultXML_Config: XML_Config = {
   ojpVersion: '2.0',
