68320865a2f5967b29ded808a3d7b2e82ff80ba3
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-05-14 11:25:38 +0200

    npm run build

--- lib/helpers/ojp-helpers.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class OJP_Helpers {
-    static buildRequestorRef(): string;
-}
--- lib/trip/trip.d.ts
+++ lib/trip/trip.d.ts
@@ -2,13 +2,11 @@ import { XMLElement } from 'xmlbuilder';
 import { TripStats } from '../types/trip-stats';
 import { TripLeg } from './leg/trip-leg';
 import { TreeNode } from '../xml/tree-node';
-import { TripFareResult } from '../fare/fare';
 import { XML_Config } from '../types/_all';
 export declare class Trip {
     id: string;
     legs: TripLeg[];
     stats: TripStats;
-    tripFareResults: TripFareResult[];
     constructor(tripID: string, legs: TripLeg[], tripStats: TripStats);
     static initFromTreeNode(treeNode: TreeNode): Trip | null;
     computeDepartureTime(): Date | null;
