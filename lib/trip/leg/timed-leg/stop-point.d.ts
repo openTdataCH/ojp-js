e0e6070631434036abf83bef79556fa8eada8eb6
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-05-23 18:03:35 +0200

    npm run build

--- lib/trip/leg/timed-leg/stop-point.d.ts
+++ lib/trip/leg/timed-leg/stop-point.d.ts
@@ -3,9 +3,7 @@ import { StopPointTime } from "./stop-point-time";
 import { StopPointType } from "../../../types/stop-point-type";
 import { PtSituationElement } from "../../../situation/situation-element";
 import { TreeNode } from "../../../xml/tree-node";
-import { FareClassType, OccupancyLevel } from "../../../types/_all";
-type VehicleAccessType = 'PLATFORM_ACCESS_WITHOUT_ASSISTANCE' | 'PLATFORM_ACCESS_WITH_ASSISTANCE' | 'PLATFORM_ACCESS_WITH_ASSISTANCE_WHEN_NOTIFIED' | 'PLATFORM_NOT_WHEELCHAIR_ACCESSIBLE' | 'ALTERNATIVE_TRANSPORT' | 'NO_DATA';
-type MapFareClassOccupancy = Record<FareClassType, OccupancyLevel | null>;
+import { MapFareClassOccupancy } from "../../../types/_all";
 export declare class StopPoint {
     stopPointType: StopPointType;
     location: Location;
@@ -17,11 +15,9 @@ export declare class StopPoint {
     isNotServicedStop: boolean | null;
     siriSituationIds: string[];
     siriSituations: PtSituationElement[];
-    vehicleAccessType: VehicleAccessType | null;
+    vehicleAccessType: string | null;
     mapFareClassOccupancy: MapFareClassOccupancy;
     constructor(stopPointType: StopPointType, location: Location, arrivalData: StopPointTime | null, departureData: StopPointTime | null, plannedPlatform: string | null, sequenceOrder: number | null);
     static initWithTreeNode(treeNode: TreeNode, stopPointType: StopPointType): StopPoint | null;
-    private static computePlatformAssistance;
     private static computeMapFareClassOccupancy;
 }
-export {};
---
+++ lib/types/index.d.ts
@@ -0,0 +1,10 @@
+export { UseRealtimeDataEnumeration, FareClassType, OccupancyLevel, MapFareClassOccupancy } from './_all';
+export * from './lir-restrictions.type';
+export * from './mode-of-transport.type';
+export * from './individual-mode.types';
+export * from './journey-points';
+export * from './language-type';
+export * from './stage-config';
+export * from './stop-event-type';
+export * from './stop-point-type';
+export * from './trip-mode-type';
