c1ea434a3627028858d384d7734b785bf431f20e
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-06-05 08:35:20 +0200

    No more need to export

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,6 +1,5 @@
 export * from './location-information/location-information-request'
 export * from './stop-event-request/stop-event-request'
-export * from './trip-info-request/trip-info-request'
 export * from './trips-request/trips-request'
 
 export * from './types/request-info.type'
