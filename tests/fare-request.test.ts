d7050d8630146b9d58b18589b3694d2dfdcb17a2
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-06-11 17:09:28 +0200

    Adds FareRequest tests

---
+++ tests/fare-request.test.ts
@@ -0,0 +1,50 @@
+import { FileHelpers } from './helpers/file-helpers';
+
+import * as OJP_Types from 'ojp-shared-types';
+import * as OJP from '../src'
+import { OJP_Helpers } from './helpers/ojp-test.helpers';
+import { XML_Config } from '../src/types/_all';
+
+describe('OJP Test TripRequest Response', () => {
+  let response1: OJP_Types.FareDeliverySchema;
+  let response2: OJP_Types.FareDeliverySchema;
+
+  beforeAll(async () => {
+    const XML_BuilderConfigOJPv1: XML_Config = {
+      ojpVersion: '1.0',
+      defaultNS: 'siri',
+      mapNS: {
+          'ojp': 'http://www.vdv.de/ojp',
+          'siri': 'http://www.siri.org.uk/siri',
+      },
+    };
+
+    const ojp = OJP_Helpers.DefaultSDK('de', XML_BuilderConfigOJPv1);
+    
+    const mockXML_1 = FileHelpers.loadMockXML('fare-response.xml');
+    const mockRequest1 = OJP.FareRequest.initWithResponseMock(mockXML_1);
+    const ojpResponse1 = await ojp.fetchFareRequestResponse(mockRequest1);
+    if (ojpResponse1.ok) {
+      response1 = ojpResponse1.value;
+    }
+
+    const mockXML_2 = FileHelpers.loadMockXML('fare-response-single-result.xml');
+    const mockRequest2 = OJP.FareRequest.initWithResponseMock(mockXML_2);
+    const ojpResponse2 = await ojp.fetchFareRequestResponse(mockRequest2);
+    if (ojpResponse2.ok) {
+      response2 = ojpResponse2.value;
+    }
+  });
+
+  test('Test FareRequest - single result', () => {
+    expect(response2.fareResult.length).toBe(1);
+    // console.log(response1.fareResult.length);
+    // console.log(response2.fareResult.length);
+  });
+
+  test('Test FareRequest - response model', () => {
+    const fareProduct = response1.fareResult[0].tripFareResult[0].fareProduct[0];
+    expect(fareProduct.price).toBe(12.2);
+    expect(fareProduct.travelClass).toBe('second');
+  });
+});
