3a4f1221ad1ba11ec6ea97e1506da35265413d4f
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-06-11 17:07:42 +0200

    Adds XML_Config

--- tests/helpers/ojp-test.helpers.ts
+++ tests/helpers/ojp-test.helpers.ts
@@ -1,13 +1,15 @@
 import * as OJP from '../../src';
+import { DefaultXML_Config } from '../../src/constants';
+import { XML_Config } from '../../src/types/_all';
 
 export class OJP_Helpers {
-  public static DefaultSDK(language: OJP.Language = 'de'): OJP.SDK {
+  public static DefaultSDK(language: OJP.Language = 'de', xmlConfig: XML_Config = DefaultXML_Config): OJP.SDK {
     const config: OJP.HTTPConfig = {
       url: 'https://endpoint.com',
       authToken: null,
     };
     
-    const sdk = new OJP.SDK('test.requestorRef', config, language);
+    const sdk = new OJP.SDK('test.requestorRef', config, language, xmlConfig);
     return sdk;
   }
 }
