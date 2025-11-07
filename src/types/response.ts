a25ffdcbc6d4ac5ed5d452f8379ef1897b90e613
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-07 14:20:38 +0100

    Adds Response types

---
+++ src/types/response.ts
@@ -0,0 +1,12 @@
+import * as OJP_Types from 'ojp-shared-types';
+
+type ResponseOk<T> = { ok: true; value: T };
+type ResponseError<E> = { ok: false; error: E };
+type OJP_Response<T, E> = ResponseOk<T> | ResponseError<E>;
+
+export type TripRequestResponse = OJP_Response<OJP_Types.TripDeliverySchema, Error>;
+export type LocationInformationRequestResponse = OJP_Response<OJP_Types.LocationInformationDeliverySchema, Error>;
+export type StopEventRequestResponse = OJP_Response<OJP_Types.StopEventDeliverySchema, Error>;
+export type TripRefineRequestResponse = OJP_Response<OJP_Types.TRR_DeliverySchema, Error>;
+export type TripInfoRequestResponse = OJP_Response<OJP_Types.TripInfoDeliverySchema | OJP_Types.OJPv1_TripInfoDeliverySchema, Error>;
+export type FareRequestResponse = OJP_Response<OJP_Types.FareDeliverySchema, Error>;
