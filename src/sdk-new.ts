7c59b665e785d0aad20ab7e3ea632aaf83704737
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-08 19:17:35 +0100

    Use a registry to map the requests

--- src/sdk-new.ts
+++ src/sdk-new.ts
@@ -6,6 +6,18 @@ type Version = 'v1' | 'v2';
 type LocationInformationRequestClass<V extends Version> =
   V extends 'v2' ? typeof LocationInformationRequest : typeof OJPv1_LocationInformationRequest;
 
+const registry = {
+  v1: {
+    LocationInformationRequest: OJPv1_LocationInformationRequest,
+  },
+  v2: {
+    LocationInformationRequest: LocationInformationRequest,
+  },
+} as const;
+type Registry = typeof registry;
+type RequestKeys = keyof Registry['v1'];
+type ClassFor<V extends Version, K extends RequestKeys> = Registry[V][K];
+
 export class SDK<V extends Version = 'v2'> {
   private readonly version: V;
 
@@ -15,5 +27,9 @@ export class SDK<V extends Version = 'v2'> {
 
   public get LocationInformationRequest(): LocationInformationRequestClass<V> {
     return (this.version === 'v2' ? LocationInformationRequest : OJPv1_LocationInformationRequest) as LocationInformationRequestClass<V>;
+  }
+
+  get requests(): { [K in RequestKeys]: ClassFor<V, K> } {
+    return registry[this.version] as { [K in RequestKeys]: ClassFor<V, K> };
   }
 }
