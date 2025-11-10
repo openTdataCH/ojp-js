985d8f7aacb7b5d9e2a2bc32da9c539ac8acbf52
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-10 21:16:17 +0100

    Order

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,13 +1,14 @@
-import { OJPv1_LocationInformationRequest } from "../versions/legacy/v1/requests/lir";
 import { LocationInformationRequest } from "../versions/current/requests/lir";
+import { OJPv1_LocationInformationRequest } from "../versions/legacy/v1/requests/lir";
 
 import { TripInfoRequest } from "../versions/current/requests/tir";
 import { OJPv1_TripInfoRequest } from "../versions/legacy/v1/requests/tir";
 
-import { TripRequest } from "../versions/current/requests/tr";
-import { OJPv1_TripRequest } from "../versions/legacy/v1/requests/tr";
 import { StopEventRequest } from "../versions/current/requests/ser";
 import { OJPv1_StopEventRequest } from "../versions/legacy/v1/requests/ser";
+
+import { TripRequest } from "../versions/current/requests/tr";
+import { OJPv1_TripRequest } from "../versions/legacy/v1/requests/tr";
 
 export type OJP_RequestType = 
     LocationInformationRequest | OJPv1_LocationInformationRequest |
