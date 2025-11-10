0571ec00b7b4a07cc819688dcba0f19d234d4e2f
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-10 08:38:37 +0100

    Adds OJPv1_LocationInformationRequestResponse

--- src/types/response.ts
+++ src/types/response.ts
@@ -5,7 +5,10 @@ type ResponseError<E> = { ok: false; error: E };
 type OJP_Response<T, E> = ResponseOk<T> | ResponseError<E>;
 
 export type TripRequestResponse = OJP_Response<OJP_Types.TripDeliverySchema, Error>;
+
 export type LocationInformationRequestResponse = OJP_Response<OJP_Types.LocationInformationDeliverySchema, Error>;
+export type OJPv1_LocationInformationRequestResponse = OJP_Response<OJP_Types.OJPv1_LocationInformationDeliverySchema, Error>;
+
 export type StopEventRequestResponse = OJP_Response<OJP_Types.StopEventDeliverySchema, Error>;
 export type TripRefineRequestResponse = OJP_Response<OJP_Types.TRR_DeliverySchema, Error>;
 export type TripInfoRequestResponse = OJP_Response<OJP_Types.TripInfoDeliverySchema | OJP_Types.OJPv1_TripInfoDeliverySchema, Error>;
