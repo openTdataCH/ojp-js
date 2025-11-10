a449023c17d9581a2580c12c3349911aae1d1f68
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-10 21:18:21 +0100

    Adds TRR to SDK.requests

--- src/sdk.ts
+++ src/sdk.ts
@@ -3,14 +3,16 @@ import { HTTPConfig, Language, OJP_VERSION } from './types/_all';
 import { LocationInformationRequest } from './versions/current/requests/lir';
 import { StopEventRequest } from "./versions/current/requests/ser";
 import { TripInfoRequest } from "./versions/current/requests/tir";
+import { TripRefineRequest } from "./versions/current/requests/trr";
 import { TripRequest } from "./versions/current/requests/tr";
 
 import { OJPv1_LocationInformationRequest } from './versions/legacy/v1/requests/lir';
 import { OJPv1_StopEventRequest } from "./versions/legacy/v1/requests/ser";
 import { OJPv1_TripInfoRequest } from "./versions/legacy/v1/requests/tir";
+import { FakeOJPv1_TripRefineRequest } from "./versions/legacy/v1/requests/trr";
 import { OJPv1_TripRequest } from "./versions/legacy/v1/requests/tr";
 
-type RequestKey = 'LocationInformationRequest' | 'StopEventRequest' | 'TripInfoRequest' | 'TripRequest';
+type RequestKey = 'LocationInformationRequest' | 'StopEventRequest' | 'TripInfoRequest' | 'TripRefineRequest' | 'TripRequest';
 
 // Registry of classes per version
 const builders = {
@@ -18,12 +20,14 @@ const builders = {
     LocationInformationRequest: OJPv1_LocationInformationRequest,
     StopEventRequest: OJPv1_StopEventRequest,
     TripInfoRequest: OJPv1_TripInfoRequest,
+    TripRefineRequest: TripRefineRequest,
     TripRequest: OJPv1_TripRequest,
   },
   '2.0': { 
     LocationInformationRequest: LocationInformationRequest,
     StopEventRequest: StopEventRequest,
     TripInfoRequest: TripInfoRequest,
+    TripRefineRequest: FakeOJPv1_TripRefineRequest,
     TripRequest: TripRequest,
   },
 } as const;
