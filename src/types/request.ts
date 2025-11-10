93f0a2a00130d30b2c63341c2e88a6e5b7496e7c
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-10 20:32:53 +0100

    Register TR

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,8 +1,12 @@
 import { OJPv1_LocationInformationRequest } from "../versions/legacy/v1/requests/lir";
 import { LocationInformationRequest } from "../versions/current/requests/lir";
+
 import { TripInfoRequest } from "../versions/current/requests/tir";
 import { OJPv1_TripInfoRequest } from "../versions/legacy/v1/requests/tir";
 
-export type OJP_LocationInformationRequestType = LocationInformationRequest | OJPv1_LocationInformationRequest
-export type OJP_TripInfoRequestType = TripInfoRequest | OJPv1_TripInfoRequest;
-export type OJP_RequestType = OJP_LocationInformationRequestType | OJP_TripInfoRequestType;
+import { TripRequest } from "../versions/current/requests/tr";
+import { OJPv1_TripRequest } from "../versions/legacy/v1/requests/tr";
+export type OJP_RequestType = 
+    LocationInformationRequest | OJPv1_LocationInformationRequest |
+    TripInfoRequest | OJPv1_TripInfoRequest |
+    TripRequest | OJPv1_TripRequest;
