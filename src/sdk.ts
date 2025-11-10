546877cbab905a238dd7d2636b76be268b4ff4ec
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-10 09:55:06 +0100

    sdk-new replaces sdk content

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,292 +1,37 @@
-import axios, { AxiosHeaders, AxiosRequestConfig } from "axios";
+import { HTTPConfig, Language, OJP_VERSION } from './types/_all';
 
-import * as OJP_Types from 'ojp-shared-types';
+import { LocationInformationRequest } from './versions/current/requests/lir';
+import { OJPv1_LocationInformationRequest } from './versions/legacy/v1/requests/lir';
 
-import { parseXML } from './helpers/xml/parser';
-import { PlaceResult, StopEventResult, Trip } from "./models/ojp";
-import { XML_Config, HTTPConfig, Language } from "./types/_all";
-import { FareRequest, LocationInformationRequest, StopEventRequest, TripInfoRequest, TripRefineRequest, TripRequest } from "./models/request";
-import { DefaultXML_Config } from "./constants";
-import { FareRequestResponse, LocationInformationRequestResponse, StopEventRequestResponse, TripInfoRequestResponse, TripRefineRequestResponse, TripRequestResponse } from "./types/response";
+type RequestKey = 'LocationInformationRequest';
 
-type OJP_RequestType = TripRequest | LocationInformationRequest | StopEventRequest | TripRefineRequest | FareRequest | TripInfoRequest;
+// Registry of classes per version
+const builders = {
+  '1.0': { 
+    LocationInformationRequest: OJPv1_LocationInformationRequest, 
+  },
+  '2.0': { 
+    LocationInformationRequest: LocationInformationRequest,
+  },
+} as const;
 
-export class SDK {
-  private requestorRef: string;
-  private httpConfig: HTTPConfig;
-  private language: Language;
-  private xmlConfig: XML_Config;
+type Builders = typeof builders;
+type ClassFor<V extends OJP_VERSION, K extends RequestKey> = Builders[V][K];
 
-  constructor(requestorRef: string, httpConfig: HTTPConfig, language: Language, xmlConfig: XML_Config = DefaultXML_Config) {
+export class SDK<V extends OJP_VERSION = '2.0'> {
+  public readonly version: OJP_VERSION;
+  public requestorRef: string;
+  public httpConfig: HTTPConfig;
+  public language: Language;
+
+  constructor(requestorRef: string, httpConfig: HTTPConfig, language: Language = 'en', version: OJP_VERSION = '2.0') {
     this.requestorRef = requestorRef;
-    // TODO - do some validation on the format? [0-9a-zA-Z_\.] ?
-
     this.httpConfig = httpConfig;
     this.language = language;
-    
-    this.xmlConfig = xmlConfig;
+    this.version = version;
   }
 
-  private async computeResponse(request: OJP_RequestType): Promise<string> {
-    const requestXML = (() => {
-      if (request.mockRequestXML) {
-        // console.log('TR: using mock request XML');
-        return request.mockRequestXML;
-      }
-
-      const xml = request.buildRequestXML(this.language, this.requestorRef, this.xmlConfig);
-      return xml;
-    })();
-
-    request.requestInfo.requestDateTime = new Date();
-    request.requestInfo.requestXML = requestXML;
-
-    const responseXML: string = await (async () => {
-      if (request.mockResponseXML) {
-        // console.log('TR: using mock response XML');
-        return request.mockResponseXML;
-      }
-
-      const xml = await this.fetchResponse(requestXML);
-      return xml;
-    })();
-
-    request.requestInfo.responseDateTime = new Date();
-    request.requestInfo.responseXML = responseXML;
-
-    return responseXML;
-  }
-
-  private async fetchResponse(requestXML: string): Promise<string> {
-    const headers = new AxiosHeaders();
-    headers.set('Accept', 'application/xml');
-    headers.set('Content-Type', 'application/xml');
-
-    if (this.httpConfig.authToken !== null) {
-      headers.set('Authorization', 'Bearer ' + this.httpConfig.authToken);
-    }
-
-    const requestConfig: AxiosRequestConfig = {
-      method: 'POST',
-      url: this.httpConfig.url,
-      headers: headers,
-    };
-
-    if (this.httpConfig.url.startsWith('http://localhost')) {
-      requestConfig.method = 'GET';
-    }
-
-    if (requestConfig.method === 'POST') {
-      requestConfig.data = requestXML;
-    }
-
-    const response = await axios.request(requestConfig);
-    const responseXML = response.data as string;
-
-    return responseXML;
-  }
-
-  public async fetchTripRequestResponse(request: TripRequest): Promise<TripRequestResponse> {
-    const responseXML = await this.computeResponse(request);
-
-    try {
-      const parsedObj = parseXML<{ OJP: OJP_Types.TripRequestResponseOJP }>(responseXML, 'OJP');
-      const response = parsedObj.OJP.OJPResponse.serviceDelivery.OJPTripDelivery;
-
-      return {
-        ok: true,
-        value: response,
-      };
-    } catch (error) {
-      return {
-        ok: false,
-        error: error instanceof Error ? error : new Error('Unknown error'),
-      };
-    }
-  }
-
-  public async fetchLocationInformationRequestResponse(request: LocationInformationRequest): Promise<LocationInformationRequestResponse> {
-    const responseXML = await this.computeResponse(request);
-
-    try {
-      const parsedObj = parseXML<{ OJP: OJP_Types.LocationInformationRequestResponseOJP }>(responseXML, 'OJP');
-      const response = parsedObj.OJP.OJPResponse.serviceDelivery.OJPLocationInformationDelivery;
-
-      return {
-        ok: true,
-        value: response,
-      };
-    } catch (error) {
-      return {
-        ok: false,
-        error: error instanceof Error ? error : new Error('Unknown error'),
-      };
-    }
-  }
-
-  public async fetchStopEventRequestResponse(request: StopEventRequest): Promise<StopEventRequestResponse> {
-    const responseXML = await this.computeResponse(request);
-
-    try {
-      const parsedObj = parseXML<{ OJP: OJP_Types.StopEventRequestResponseOJP }>(responseXML, 'OJP');
-      const response = parsedObj.OJP.OJPResponse.serviceDelivery.OJPStopEventDelivery;
-
-      return {
-        ok: true,
-        value: response,
-      };
-    } catch (error) {
-      return {
-        ok: false,
-        error: error instanceof Error ? error : new Error('Unknown error'),
-      };
-    }
-  }
-
-  public async fetchTripRefineRequestResponse(request: TripRefineRequest): Promise<TripRefineRequestResponse> {
-    const responseXML = await this.computeResponse(request);
-
-    try {
-      const parsedObj = parseXML<{ OJP: OJP_Types.TRR_ResponseOJP }>(responseXML, 'OJP');
-      const response = parsedObj.OJP.OJPResponse.serviceDelivery.OJPTripRefineDelivery;
-
-      return {
-        ok: true,
-        value: response,
-      };
-    } catch (error) {
-      return {
-        ok: false,
-        error: error instanceof Error ? error : new Error('Unknown error'),
-      };
-    }
-  }
-
-  public async fetchTripInfoRequestResponse(request: TripInfoRequest): Promise<TripInfoRequestResponse> {
-    const responseXML = await this.computeResponse(request);
-
-    try {
-      const parsedObj = parseXML<{ OJP: OJP_Types.TripInfoResponseOJP | OJP_Types.OJPv1_TripInfoResponseOJP }>(responseXML, 'OJP');
-      const response = parsedObj.OJP.OJPResponse.serviceDelivery.OJPTripInfoDelivery;
-
-      return {
-        ok: true,
-        value: response,
-      };
-    } catch (error) {
-      return {
-        ok: false,
-        error: error instanceof Error ? error : new Error('Unknown error'),
-      };
-    }
-  }
-
-  public async fetchFareRequestResponse(request: FareRequest): Promise<FareRequestResponse> {
-    const responseXML = await this.computeResponse(request);
-
-    try {
-      const parsedObj = parseXML<{ OJP: OJP_Types.FareResponseOJP }>(responseXML, 'OJP');
-      const response = parsedObj.OJP.OJPResponse.serviceDelivery.OJPFareDelivery;
-
-      return {
-        ok: true,
-        value: response,
-      };
-    } catch (error) {
-      return {
-        ok: false,
-        error: error instanceof Error ? error : new Error('Unknown error'),
-      };
-    }
-  }
-
-  // Deprecated methods below
-
-  public async fetchTrips(tripRequest: TripRequest): Promise<Trip[]> {
-    console.log('WARNING: deprecated method, it might be removed at a later version, use fetchTripRequestResponse() instead');
-
-    const responseXML = await this.computeResponse(tripRequest);
-    
-    const ojpPrefix = this.xmlConfig.defaultNS === 'ojp' ? '' : 'ojp:';
-
-
-    const tripPattern = '<' + ojpPrefix + 'Trip\\b[^>]*>.*?<\\/' + ojpPrefix + 'Trip>';
-    const tripRegexp = new RegExp(tripPattern, 'gs');
-    const tripMatches: string[] = responseXML.match(tripRegexp) ?? [];
-    
-    // console.log('fetchTrips - regexp matches - found ' + tripMatches.length + ' trips');
-
-    const trips: Trip[] = [];
-    tripMatches.forEach((tripXML, idx1) => {
-      const trip = Trip.initWithTripXML(tripXML);
-      trips.push(trip);
-    });
-
-    tripRequest.requestInfo.parseDateTime = new Date();
-
-    // console.log('fetchTrips - done init trips');
-
-    return trips;
-  }
-
-  public async fetchPlaceResults(lirRequest: LocationInformationRequest): Promise<PlaceResult[]> {
-    console.log('WARNING: deprecated method, it might be removed at a later version, use fetchLocationInformationRequestResponse() instead');
-
-    const responseXML = await this.computeResponse(lirRequest);
-
-    // console.log('fetchLocations ... done fetchResponse');
-
-    const placeResultMatches: string[] = responseXML.match(/<PlaceResult\b[^>]*>.*?<\/PlaceResult>/gs) ?? [];
-    
-    // console.log('fetchLocations - regexp matches - found ' + placeResultMatches.length + ' places');
-
-    const placeResults: PlaceResult[] = [];
-    placeResultMatches.forEach((nodeXML, idx1) => {
-      const placeResult = PlaceResult.initWithXML(nodeXML);
-      placeResults.push(placeResult);
-    });
-
-    lirRequest.requestInfo.parseDateTime = new Date();
-
-    return placeResults;
-  }
-
-  public async fetchStopEvents(request: StopEventRequest): Promise<StopEventResult[]> {
-    console.log('WARNING: deprecated method, it might be removed at a later version, use fetchStopEventRequestResponse() instead');
-
-    const responseXML = await this.computeResponse(request);
-
-    // console.log('fetchStopEvents ... done fetchResponse');
-
-    const resultMatches: string[] = responseXML.match(/<StopEventResult\b[^>]*>.*?<\/StopEventResult>/gs) ?? []; 
-    // console.log('fetchStopEvents - regexp matches - found ' + resultMatches.length + ' stop events');
-
-    const results: StopEventResult[] = [];
-    resultMatches.forEach((nodeXML, idx1) => {
-      const result = StopEventResult.initWithXML(nodeXML);
-      results.push(result);
-    });
-
-    request.requestInfo.parseDateTime = new Date();
-
-    return results;
-  }
-
-  public async fetchTRR_Trips(request: TripRefineRequest): Promise<Trip[]> {
-    console.log('WARNING: deprecated method, it might be removed at a later version, use fetchTripRefineRequestResponse() instead');
-
-    const responseXML = await this.computeResponse(request);
-
-    const tripMatches: string[] = responseXML.match(/<Trip\b[^>]*>.*?<\/Trip>/gs) ?? [];
-
-    const trips: Trip[] = [];
-    tripMatches.forEach((tripXML, idx1) => {
-      const trip = Trip.initWithTripXML(tripXML);
-      trips.push(trip);
-    });
-
-    request.requestInfo.parseDateTime = new Date();
-
-    return trips;
+  get requests(): { [K in RequestKey]: ClassFor<V, K> } {
+    return builders[this.version] as { [K in RequestKey]: ClassFor<V, K> };
   }
 }
