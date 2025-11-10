b7dffd710e5506b96515fa84a17445ea91f29d07
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-10 20:36:47 +0100

    Adds missing imports

--- src/sdk.ts
+++ src/sdk.ts
@@ -1,10 +1,12 @@
 import { HTTPConfig, Language, OJP_VERSION } from './types/_all';
 
 import { LocationInformationRequest } from './versions/current/requests/lir';
+import { StopEventRequest } from "./versions/current/requests/ser";
 import { TripInfoRequest } from "./versions/current/requests/tir";
 import { TripRequest } from "./versions/current/requests/tr";
 
 import { OJPv1_LocationInformationRequest } from './versions/legacy/v1/requests/lir';
+import { OJPv1_StopEventRequest } from "./versions/legacy/v1/requests/ser";
 import { OJPv1_TripInfoRequest } from "./versions/legacy/v1/requests/tir";
 import { OJPv1_TripRequest } from "./versions/legacy/v1/requests/tr";
 
