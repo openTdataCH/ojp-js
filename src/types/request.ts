aad897223959fb43ac2fd8568ab4fe517846b095
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-10 18:00:56 +0100

    Adds TIR request

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,5 +1,8 @@
 import { OJPv1_LocationInformationRequest } from "../versions/legacy/v1/requests/lir";
 import { LocationInformationRequest } from "../versions/current/requests/lir";
+import { TripInfoRequest } from "../versions/current/requests/tir";
+import { OJPv1_TripInfoRequest } from "../versions/legacy/v1/requests/tir";
 
 export type OJP_LocationInformationRequestType = LocationInformationRequest | OJPv1_LocationInformationRequest
-export type OJP_RequestType = OJP_LocationInformationRequestType;
+export type OJP_TripInfoRequestType = TripInfoRequest | OJPv1_TripInfoRequest;
+export type OJP_RequestType = OJP_LocationInformationRequestType | OJP_TripInfoRequestType;
