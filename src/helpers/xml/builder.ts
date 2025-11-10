fcfbe7d04131da867695b7b7a144aba7e3e74080
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-10 17:52:13 +0100

    No more need for this hack

--- src/helpers/xml/builder.ts
+++ src/helpers/xml/builder.ts
@@ -78,21 +78,6 @@ export function buildXML(obj: Record<string, any>, wrapperNodeName: string = 'OJ
     // capitalize first letter
     let newKey = key.charAt(0).toUpperCase() + key.slice(1);
 
-    const keysToKeep = OJP_Types.OpenAPI_Dependencies.MapModelKeepPropertiesXML[key] ?? null;
-    if (keysToKeep !== null) {
-      if (typeof value === 'object') {
-        const objKeys = Object.keys(value);
-        objKeys.forEach(objKey => {
-          if (keysToKeep.includes(objKey)) {
-            return;
-          }
-
-          // remove keys that are not in XSD
-          delete(value[objKey]);
-        });
-      }
-    }
-    
     // ensure namespaces
     const parentKey = path.at(-1) ?? null;
     if (parentKey !== null) {
