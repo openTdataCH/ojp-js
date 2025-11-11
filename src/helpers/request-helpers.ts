ee504eaaa9cabb9c9e86c003d6841e77ef195c8d
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-11 10:17:54 +0100

    No more need of this

--- src/helpers/request-helpers.ts
+++ src/helpers/request-helpers.ts
@@ -2,7 +2,6 @@ import axios, { AxiosHeaders, AxiosRequestConfig } from "axios";
 
 import { SDK } from "../sdk";
 
-import { DefaultXML_Config } from "../constants";
 import { HTTPConfig, XML_Config } from "../types/_all";
 import { OJP_RequestType } from "../types/request";
 
