e8168070234d4200d2f2d534d29d35c3bf713e1d
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-15 14:03:01 +0100

    Export all Request classes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,17 @@ export {
 
 export { HTTPConfig, Language, RequestInfo, OJP_VERSION, AnySDK } from "./types/_all";
 
+export { OJPv1_FareRequest } from './versions/legacy/v1/requests/fr';
+export { OJPv1_LocationInformationRequest } from './versions/legacy/v1/requests/lir';
+export { OJPv1_StopEventRequest } from './versions/legacy/v1/requests/ser';
+export { OJPv1_TripInfoRequest } from './versions/legacy/v1/requests/tir';
+export { OJPv1_TripRequest } from './versions/legacy/v1/requests/tr';
+export { LocationInformationRequest } from './versions/current/requests/lir';
+export { StopEventRequest } from './versions/current/requests/ser';
+export { TripInfoRequest } from './versions/current/requests/tir';
+export { TripRequest } from './versions/current/requests/tr';
+export { TripRefineRequest } from './versions/current/requests/trr';
+
 export {
   FareRequestResponse,
   LocationInformationRequestResponse,
