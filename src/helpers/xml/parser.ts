f994f6580f4314b6c149b40fd9827a9ff80f41f4
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2025-11-23 18:00:19 +0100

    Use new parser, remove old code

--- src/helpers/xml/parser.ts
+++ src/helpers/xml/parser.ts
@@ -160,27 +160,6 @@ const isArrayHandler = (tagName: string, jPath: string) => {
   return false;
 };
 
-function traverseJSON(obj: any, path: string[], callback: (key: string, value: any, path: string[]) => void) {
-  if ((typeof obj !== 'object') || (obj === null)) {
-    return;
-  }
-
-  if (Array.isArray(obj)) {
-    for (const item of obj) {
-      traverseJSON(item, path, callback);
-    }
-  } else {
-    for (const key in obj) {
-      const newPath = path.slice();
-      newPath.push(key);
-
-      callback(key, obj[key], newPath);
-
-      traverseJSON(obj[key], newPath, callback);
-    }
-  }
-}
-
 export function parseXML<T>(xml: string, parentPath: string = ''): T {
   const parser = new XMLParser({
     ignoreAttributes: false,
@@ -191,57 +170,7 @@ export function parseXML<T>(xml: string, parentPath: string = ''): T {
   });
 
   const response = parser.parse(xml) as T;
+  transformJsonInPlace(response);
 
-  traverseJSON(response, [parentPath], (key: string, value: any, path: string[]) => {
-    // console.log('traverseJSON_> ' + path.join('.') + ' k: ' + key + ' v: ' + value);
-    
-    if (typeof value === 'object') {    
-      // enforce empty arrays if the array items are not present
-      if (path.length >= 2) {
-        if (key in MapParentArrayTags) {
-          const enforceChildTags = MapParentArrayTags[key];
-          if (Array.isArray(value)) {
-            value.forEach(childValue => {
-              enforceChildTags.forEach(childTagName => {
-                childValue[childTagName] ??= [];
-              });
-            });
-          } else {
-            enforceChildTags.forEach(childTagName => {
-              value[childTagName] ??= [];
-            });
-          }
-        }
-      }
-
-      for (const key1 in value) {
-        if (typeof value[key1] === 'object') {
-          // check for #text keys that are added for text nodes that have attributes
-          if (Object.keys(value[key1]).includes('#text')) {
-            const otherKeys = Object.keys(value[key1]).filter(el => el !== '#text');
-
-            // keep attributes
-            otherKeys.forEach(otherKey => {
-              const newKey = key1 + otherKey;
-              value[newKey] = value[key1][otherKey];
-            });
-            
-            // replace the object with literal value of #text
-            value[key1] = value[key1]['#text'];
-          }
-        }
-
-        // for .text properties we need the before step (#text parsing) to be executed first
-        const lastItem = (path.at(-1) ?? '');
-        const stringKey = lastItem + '.' + key1;
-        if (stringKey in OJP_Types.OpenAPI_Dependencies.MapStringValues) {
-          // fast-xml-parser attempts to converts everything
-          //    conform to schema id needed, i.e. String values
-          value[key1] = String(value[key1]);
-        }
-      }
-    }
-  });
-  
   return response;
 }
