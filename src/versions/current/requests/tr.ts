ac3b2b14dda3e3f1009248a0b16fe99f324b0d30
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-01-30 12:30:43 +0100

    OJP v2 DefaultRequestParams

--- src/versions/current/requests/tr.ts
+++ src/versions/current/requests/tr.ts
@@ -35,6 +35,25 @@ export class TripRequest extends SharedTripRequest<{ fetchResponse: TripRequestR
     };
   }
 
+  private static DefaultRequestParams(): OJP_Types.TripParamsSchema {
+    const requestParams: OJP_Types.TripParamsSchema = {
+      modeAndModeOfOperationFilter: [],
+      
+      numberOfResults: 5,
+      numberOfResultsBefore: undefined,
+      numberOfResultsAfter: undefined,
+
+      useRealtimeData: 'explanatory',
+
+      includeAllRestrictedLines: true,
+      includeTrackSections: true,
+      includeLegProjection: false,
+      includeIntermediateStops: true,
+    };
+
+    return requestParams;
+  }
+
   // Used by Base.initWithRequestMock / initWithResponseMock
   public static Default() {
     const date = new Date();
