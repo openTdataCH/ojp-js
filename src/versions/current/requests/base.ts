425f43d4e37a295f9793aa10b2dcf88bf45365c5
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2026-02-25 22:52:49 +0100

    Typedoc shared Initialization category

--- src/versions/current/requests/base.ts
+++ src/versions/current/requests/base.ts
@@ -23,12 +23,44 @@ export abstract class BaseRequest<S extends ResultSpec> {
     this.mockResponseXML = null;
   }
 
+  /**
+   * Creates a new instance of the request class with a mock request XML string
+   * 
+   * This method is used for testing purposes to simulate API requests with predefined XML payloads.
+   * When this mock is set, the request will use the provided XML instead of building a new one.
+   * 
+   * @param mockText - The XML string to use as the mock request
+   * @returns A new instance of the request class with the mock set
+   * 
+   * @example
+   * ```typescript
+   * const mockRequest = LocationInformationRequest.initWithRequestMock('<OJP>...</OJP>');
+   * ```
+   * 
+   * @group Initialization
+   */
   public static initWithRequestMock<T_This extends { Default(): any }>(this: T_This, mockText: string): ReturnType<T_This['Default']> {
     const instance = this.Default();
     (instance as BaseRequest<any>).mockRequestXML = mockText;
     return instance as ReturnType<T_This['Default']>;
   }
 
+  /**
+   * Creates a new instance of the request class with a mock response XML string
+   * 
+   * This method is used for testing purposes to simulate API responses with predefined XML payloads.
+   * When this mock is set, the request will use the provided XML response instead of making actual API calls.
+   * 
+   * @param mockText - The XML string to use as the mock response
+   * @returns A new instance of the request class with the mock set
+   * 
+   * @example
+   * ```typescript
+   * const mockRequest = LocationInformationRequest.initWithResponseMock('<OJP>...</OJP>');
+   * ```
+   * 
+   * @group Initialization
+   */
   public static initWithResponseMock<T_This extends { Default(): any }>(this: T_This, mockText: string): ReturnType<T_This['Default']> {
     const instance = this.Default();
     (instance as BaseRequest<any>).mockResponseXML = mockText;
